db3937f7c2402e0c8b8f3141bd0e0de940695ceb
pawan <spavandsp@gmail.com>
2025-09-15 21:55:54 +0530

    change gemini model

--- src/services/genAI.ts
+++ src/services/genAI.ts
@@ -2,4 +2,4 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import { GEMINI_API_KEY } from "../config";
 
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY!);
-export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+export const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
